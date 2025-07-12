90baff8e8440d66444cf8a5b2d1644eabf2cbcf6
dsync89 <dsync89@yahoo.com>
2025-07-12 13:31:12 +0800

    feat: update cassette template

--- templates/cassette.lua
+++ templates/cassette.lua
@@ -1,4 +1,4 @@
--- softlistname.lua
+-- _cass.lua
 
 -- Load common autoboot functions
 local common_autoboot = dofile("autoboot_scripts/autoboot_common.lua")
@@ -7,98 +7,40 @@ local common_autoboot = dofile("autoboot_scripts/autoboot_common.lua")
 local button = {}
 local frame_num = 0
 
+-- --- Cassette Loading Handler Setup ---
+local CASSETTE_SLOT = 3
+local CASSETTE_DEVICE_TAG = manager.machine.images:at(CASSETTE_SLOT).device.tag
+local cassette_handler = common_autoboot.create_cassette_handler(CASSETTE_DEVICE_TAG) 
+
 common_autoboot.populate_buttons(button)
 
--- Print the button table contents for debugging
--- common_autoboot.print_buttons(button)
+common_autoboot.print_image_info()
 
 -- --- Constants for this specific script ---
 local BUTTON_PRESS_DURATION = common_autoboot.DEFAULT_BUTTON_PRESS_DURATION
 local CASSETTE_MOTOR_OFF_DELAY_FRAMES = common_autoboot.DEFAULT_CASSETTE_MOTOR_OFF_DELAY_FRAMES
 
 local function boot_default(cassette_handler)
-    common_autoboot.type_at_frame(frame_num, "CLOAD\n", 100, BUTTON_PRESS_DURATION)
-    common_autoboot.play_cassette_at_frame(frame_num, ":cassette", 200)
+    common_autoboot.type_at_frame(frame_num, "\n", 300)
+    common_autoboot.type_at_frame(frame_num, "LOAD\n", 400)
+    common_autoboot.play_cassette_at_frame(frame_num, CASSETTE_DEVICE_TAG, 500)
 
     local cassette_load_done = cassette_handler(frame_num, CASSETTE_MOTOR_OFF_DELAY_FRAMES)
 
     if cassette_load_done then
-        emu.keypost('RUN\n')
+        -- emu.keypost('RUN\n')
     end
 end
-
-local function create_boot_type_1_sequence(title_to_type) -- New name for clarity
-    return function(cassette_handler_arg) -- This is the function that process_frame will call every frame
-        common_autoboot.type_at_frame(frame_num, "L\n", 100)
-        common_autoboot.type_at_frame(frame_num, "\n", 200)
-        common_autoboot.type_at_frame(frame_num, "SYSTEM\n", 300)
-        common_autoboot.type_at_frame(frame_num, title_to_type .. "\n", 400)
-        common_autoboot.play_cassette_at_frame(frame_num, ":cassette", 500)
-
-        local cassette_load_done = cassette_handler_arg(frame_num, CASSETTE_MOTOR_OFF_DELAY_FRAMES)
-
-        if cassette_load_done then
-            emu.keypost('/\n')
-        end
-    end
-end
-
-local function create_boot_type_2_sequence() -- New name for clarity
-    return function(cassette_handler_arg) -- This is the function that process_frame will call every frame
-        common_autoboot.type_at_frame(frame_num, "L\n", 100)
-        common_autoboot.type_at_frame(frame_num, "\n", 200)
-        common_autoboot.type_at_frame(frame_num, "CLOAD\n", 300)
-        common_autoboot.play_cassette_at_frame(frame_num, ":cassette", 400)
-
-        local cassette_load_done = cassette_handler_arg(frame_num, CASSETTE_MOTOR_OFF_DELAY_FRAMES)
-
-        if cassette_load_done then
-            emu.keypost('RUN\n')
-        end
-    end
-end
-
-local function boot_dtrap(cassette_handler)
-    common_autoboot.type_at_frame(frame_num, "L\n", 100, BUTTON_PRESS_DURATION)
-    common_autoboot.type_at_frame(frame_num, "\n", 200, BUTTON_PRESS_DURATION)
-    common_autoboot.type_at_frame(frame_num, "32640\n", 300, BUTTON_PRESS_DURATION)
-    common_autoboot.type_at_frame(frame_num, "CLOAD\n", 400, BUTTON_PRESS_DURATION)
-    common_autoboot.play_cassette_at_frame(frame_num, ":cassette", 500)
-
-    local cassette_load_done = cassette_handler(frame_num, CASSETTE_MOTOR_OFF_DELAY_FRAMES)
-
-    if cassette_load_done then
-        emu.keypost('RUN\n')
-    end
-end
-
--- local function boot_adv03(cassette_handler)
---     common_autoboot.type_at_frame(frame_num, "L\n", 100, BUTTON_PRESS_DURATION)
---     common_autoboot.type_at_frame(frame_num, "\n", 200, BUTTON_PRESS_DURATION)
---     common_autoboot.type_at_frame(frame_num, "SYSTEM\n", 300, BUTTON_PRESS_DURATION)
---     common_autoboot.type_at_frame(frame_num, "MISSIO\n", 300, BUTTON_PRESS_DURATION)
---     common_autoboot.play_cassette_at_frame(frame_num, ":cassette", 400)
-
---     local cassette_load_done = cassette_handler(frame_num, CASSETTE_MOTOR_OFF_DELAY_FRAMES)
-
---     if cassette_load_done then
---         emu.keypost('/\n')
---     end
--- end
-
 
 -- --- Main Script Logic ---
 
 -- Determine the currently loaded software name
-local current_software_name = manager.machine.images:at(1).filename
+local current_software_name = manager.machine.images:at(CASSETTE_SLOT).filename
 
 -- Map software names to their respective boot functions
 local boot_sequences = {
-    ["default"] = boot_default,
+    ['default'] = boot_default,
 }
-
--- --- Cassette Loading Handler Setup ---
-local cassette_handler = common_autoboot.create_cassette_handler(":cassette") 
 
 -- MAME machine frame notification callback
 local function process_frame()
@@ -117,10 +59,8 @@ local function process_frame()
         end        
     end
 
-    -- --- Print current frame number every 100 frames ---
-    if frame_num % 100 == 0 then -- The modulo operator (%) gives the remainder of a division
-        emu.print_info("Current Frame: " .. frame_num)
-    end    
+    --- DEBUG: Print current frame number every 100 frames ---
+    common_autoboot.debug_frame_num(frame_num)
 
     -- Execute the relevant boot sequence based on the loaded software
     local boot_function = boot_sequences[current_software_name]
